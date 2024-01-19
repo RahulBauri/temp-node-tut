705b89b91e7ac746a0cdb37b2648aec99aaa046a
Rahul Bauri <kumarrahulbauri2000@gmail.com>
2024-01-19 12:00:53 +0530

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,17 @@
+// CommonJS
+// every file in node is a module (by default)
+// Modules - Encapsulated code (only share minimum)
+
+const names = require('./4-names');
+// console.log(names);
+
+const sayHi = require('./5-utils');
+
+const data = require('./6-alternative-flavour');
+// console.log(data);
+
+require('./7-mind-grenade');
+
+sayHi('susan');
+sayHi(names.john);
+sayHi(names.peter);
---
+++ 4-names.js
@@ -0,0 +1,9 @@
+// local (don't share)
+const secret = 'SUPER SECRET';
+// share
+const john = 'john';
+const peter = 'peter';
+
+// console.log(module);
+
+module.exports = { john: john, peter };
